d41e9791ca0a94f793df7eed8462b9df1c98e4dc
Daniel <blaindoctor@gmail.com>
2022-06-22 12:39:56 -0700

    Work in progress - Outputs remaining every second.
    TODO:
    * Clear CLI every second before printing again
    * Better countdown font

--- src/command_line.rs
+++ src/command_line.rs
@@ -6,28 +6,20 @@ const DIVIDER_STRING: &str = "-----------------";
 #[derive(Parser)]
 #[clap(author = "Daniel_Panuco")]
 #[clap(version = "0.01.12")]
-#[clap(about = "Command line tool for encrypting files")]
+#[clap(about = "Command line timer")]
 #[clap(long_about = None)]
 #[derive(Debug)]
 
 pub struct Args {
-    #[clap(short = 'f', long = "file", value_parser)]
-    pub file: std::path::PathBuf,
-
-    #[clap(short = 'k', long = "key", value_parser)]
-    pub key: std::path::PathBuf,
-
-    #[clap(short = 'r', long = "random", value_parser)]
-    pub rand: Option<i32>,
+    // #[clap(short = 't', long = "time", value_parser)]
+    pub time: String,
 }
 
 impl std::fmt::Display for Args {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f,
                "Arguments\n{}\n\
-            Plain Text Path: {:?}\n\
-            Key Path: {:?}\n\
-            Random: {:?}",
-               DIVIDER_STRING, self.file, self.key, self.rand)
+                Time: {}",
+               DIVIDER_STRING, self.time)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,72 @@
 mod command_line;
 
 use clap::Parser;
+use regex::Regex;
+use std::time;
+
+const DATE_PATTERN: &str = r"((?P<years>\d+)y)?\
+(?P<days>\d+)d)?\
+((?P<hours>\d+)h)?\
+((?P<minutes>\d+)m)?\
+((?P<seconds>\d+)s)?";
+use std::thread;
 
 fn main() {
     let args: command_line::Args = command_line::Args::parse();
+    let start = time::Instant::now();
+    println!("{}", DATE_PATTERN);
 
-    println!("{}", args.time);
+    let duration = parse_input(args.time);
 
+    println!("{:?}", duration);
+    println!("{:?}", start);
+
+    loop {
+        let elapsed = start.elapsed();
+        println!("{}", elapsed.as_secs());
+
+        if duration < elapsed {
+            break;
+        }
+
+        let remain = duration.as_secs() - elapsed.as_secs();
+        std::thread::sleep(time::Duration::from_millis(1000));
+        println!("Time Left: {}", remain);
+    }
 }
 
+fn parse_input(duration: String) -> time::Duration {
+    let re = Regex::new(r"((?P<years>\d+)y)?((?P<days>\d+)d)?
+((?P<hours>\d+)h)?
+((?P<minutes>\d+)m)?
+((?P<seconds>\d+)s)?")
+        .unwrap();
+    //let re = Regex::new(DATE_PATTERN).unwrap();
+    let caps = re.captures(&*duration).unwrap();
 
+    let y: u64 = caps.name("years").map_or(0, |m| m.as_str().parse().unwrap());
+    let d: u64 = caps.name("days").map_or(0, |m| m.as_str().parse().unwrap());
+    let h: u64 = caps.name("hours").map_or(0, |m| m.as_str().parse().unwrap());
+    let m: u64 = caps.name("minutes").map_or(0, |m| m.as_str().parse().unwrap());
+    let s: u64 = caps.name("seconds").map_or(0, |m| m.as_str().parse().unwrap());
+    time::Duration::new(36000 * h + 60 * m + s, 0)
+}
+
+fn temp() {
+    let args: command_line::Args = command_line::Args::parse();
+    let re = Regex::new(DATE_PATTERN).unwrap();
+
+    println!("Time: {}", args.time);
+
+    let caps = re.captures(&*args.time).unwrap();
+    println!("Length of capture: {}", caps.len());
+
+    // let caps = re.captures(&*args.time).unwrap();
+    for cap in re.captures_iter(&*args.time) {
+        println!("{}", cap.get(4).unwrap().as_str());
+    };
+
+}
 fn turn_time_to_seconds(time: String) -> i32 {
     unimplemented!()
 }
